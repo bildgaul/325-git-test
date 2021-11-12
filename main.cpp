87f049e056d8e285034b8a118de07513a543bdf0
bildgaul <bildgaul@ut.utm.edu>
2021-11-11 19:21:18 -0600

    main is now a summation program!

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 using namespace std;
 
+int sum(int n){
+  int s = 0;
+  for (int i = 1; i <= n; i++)
+    s += i;
+  return s;
+}
+
 int main(){
-  cout << "Hello World!" << endl;
+  cout << "Enter a number: ";
+  int n;
+  cin >> n;
+  cout << "The sum from the number 1 to your number is: " << sum(n) << endl;
+  
   return 0;
 }
